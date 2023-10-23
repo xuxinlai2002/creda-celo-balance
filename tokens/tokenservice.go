3f6b0d81b955dd0e2b3567b3f30b5168d32d9aa9
poon <heroopan@gmail.com>
2023-10-23 17:47:36 +0800

    add hook interceptor for os signals

--- tokens/tokenservice.go
+++ tokens/tokenservice.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/xuxinlai2002/creda-celo-balance/signal"
+	"github.com/xuxinlai2002/creda-celo-balance/utils"
 	"math/big"
+	"sync"
 	"time"
 
 	"github.com/celo-org/celo-blockchain/common"
@@ -20,9 +23,10 @@ type TokenService struct {
 	cfg      *config.Config
 	records  map[string][]*ctypes.TokenRecord
 	database *db.PostgresDB
+	wg       *sync.WaitGroup
 }
 
-func NewService(cfg *config.Config) (*TokenService, error) {
+func NewService(cfg *config.Config, wg *sync.WaitGroup) (*TokenService, error) {
 	cli, err := client.Dial(cfg.HTTP)
 	if err != nil {
 		return nil, err
@@ -38,12 +42,16 @@ func NewService(cfg *config.Config) (*TokenService, error) {
 		cfg:      cfg,
 		records:  make(map[string][]*ctypes.TokenRecord),
 		database: database,
+		wg:       wg,
 	}, nil
 }
 
-func (s *TokenService) Start() error {
+func (s *TokenService) Start(interceptor signal.Interceptor) error {
+	s.wg.Add(1)
+
 	go func() {
-		s.processERC20Tokens()
+		defer s.wg.Done()
+		s.processERC20Tokens(interceptor)
 	}()
 
 	return nil
@@ -64,13 +72,21 @@ func (s *TokenService) persistToDB(date string, records []*ctypes.TokenRecord) e
 	return nil
 }
 
-func (s *TokenService) processERC20Tokens() {
+func (s *TokenService) processERC20Tokens(interceptor signal.Interceptor) {
 	tokens := ERC20Tokens
 	distance := uint64(10000)
 	toBlock := uint64(0)
 	logTransferSig := []byte("Transfer(address,address,uint256)")
 
-	for i := s.cfg.StartBlock; i < s.cfg.EndBlock; i = toBlock + 1 {
+	startHeight := s.cfg.StartBlock
+
+	progress, err := utils.GetTokenCurrentHeight()
+	fmt.Println("token start height: ", progress)
+	if err == nil && progress > startHeight {
+		startHeight = progress + 1
+	}
+
+	for i := startHeight; i < s.cfg.EndBlock; i = toBlock + 1 {
 		if i+distance < s.cfg.EndBlock {
 			toBlock = i + distance
 		} else {
@@ -78,63 +94,73 @@ func (s *TokenService) processERC20Tokens() {
 		}
 		fmt.Printf("pull block from %v to %v\n", i, toBlock)
 		for address, tokenInfo := range tokens {
-			query := s.cli.BuildQuery(address, logTransferSig, big.NewInt(0).SetUint64(i), big.NewInt(0).SetUint64(toBlock))
-			logs, err := s.cli.FilterLogs(context.Background(), query)
-			if err != nil {
-				fmt.Printf("filter logs failed, error: %v\n", err)
-			} else if len(logs) > 0 {
-				fmt.Printf("addr: %v, len(logs): %v\n", address, len(logs))
-				//fmt.Println("Date,CoinID,BlockNumber,Time,TxHash,From,To,Value")
-				for _, vlog := range logs {
-					bn := big.NewInt(0)
-					bn.SetUint64(vlog.BlockNumber)
-					b, err := s.cli.BlockByNumber(context.Background(), bn)
-					if err != nil {
-						fmt.Printf("rpc.BlockByNumber err: %v\n", err)
-					} else {
-						tr := &ctypes.TokenRecord{
-							CoinID:      tokenInfo.CoinID,
-							BlockNumber: vlog.BlockNumber,
-							Timestamp:   b.Header().Time,
-							TxHash:      vlog.TxHash,
-							From:        common.HexToAddress(vlog.Topics[1].Hex()),
-							To:          common.HexToAddress(vlog.Topics[2].Hex()),
-							Value:       big.NewInt(0).SetBytes(vlog.Data),
-						}
-						if tr.Value.Cmp(big.NewInt(0)) <= 0 {
-							continue
-						}
-
-						t := time.Unix(int64(tr.Timestamp), 0)
-						date := fmt.Sprintf("%04d%02d%02d", t.Year(), int(t.Month()), t.Day())
-
-						//fmt.Printf("-> %v,%v(%v),%v,%v,%s,%s,%s,%d\n",
-						//	date, tr.CoinID, tokenInfo.Name, tr.BlockNumber, tr.Timestamp, tr.TxHash, tr.From, tr.To, tr.Value)
-
-						_, ok := s.records[date]
-						if ok {
-							s.records[date] = append(s.records[date], tr)
+			select {
+			default:
+				query := s.cli.BuildQuery(address, logTransferSig, big.NewInt(0).SetUint64(i), big.NewInt(0).SetUint64(toBlock))
+				logs, err := s.cli.FilterLogs(context.Background(), query)
+				if err != nil {
+					fmt.Printf("filter logs failed, error: %v\n", err)
+				} else if len(logs) > 0 {
+					fmt.Printf("addr: %v, len(logs): %v\n", address, len(logs))
+					//fmt.Println("Date,CoinID,BlockNumber,Time,TxHash,From,To,Value")
+					for _, vlog := range logs {
+						bn := big.NewInt(0)
+						bn.SetUint64(vlog.BlockNumber)
+						b, err := s.cli.BlockByNumber(context.Background(), bn)
+						if err != nil {
+							fmt.Printf("rpc.BlockByNumber err: %v\n", err)
 						} else {
-							if len(s.records) > 0 {
-								for k, v := range s.records {
-									records := v
-									delete(s.records, k)
-
-									go func() {
-										if err := s.persistToDB(k, records); err != nil {
-											fmt.Printf("persist token event to db err: %v\n", err)
-										}
-									}()
-								}
+							tr := &ctypes.TokenRecord{
+								CoinID:      tokenInfo.CoinID,
+								BlockNumber: vlog.BlockNumber,
+								Timestamp:   b.Header().Time,
+								TxHash:      vlog.TxHash,
+								From:        common.HexToAddress(vlog.Topics[1].Hex()),
+								To:          common.HexToAddress(vlog.Topics[2].Hex()),
+								Value:       big.NewInt(0).SetBytes(vlog.Data),
 							}
-							s.records[date] = []*ctypes.TokenRecord{tr}
+							if tr.Value.Cmp(big.NewInt(0)) <= 0 {
+								continue
+							}
+
+							t := time.Unix(int64(tr.Timestamp), 0)
+							date := fmt.Sprintf("%04d%02d%02d", t.Year(), int(t.Month()), t.Day())
+
+							//fmt.Printf("-> %v,%v(%v),%v,%v,%s,%s,%s,%d\n",
+							//	date, tr.CoinID, tokenInfo.Name, tr.BlockNumber, tr.Timestamp, tr.TxHash, tr.From, tr.To, tr.Value)
+
+							if _, ok := s.records[date]; ok {
+								s.records[date] = append(s.records[date], tr)
+							} else {
+								if len(s.records) > 0 {
+									for k, v := range s.records {
+										records := v
+										delete(s.records, k)
+
+										s.wg.Add(1)
+										go func() {
+											s.wg.Done()
+											if err := s.persistToDB(k, records); err != nil {
+												fmt.Printf("persist token event to db err: %v\n", err)
+											}
+										}()
+									}
+								}
+								s.records[date] = []*ctypes.TokenRecord{tr}
+							}
 						}
 					}
+					utils.WriteTokenCurrentHeight(toBlock)
 				}
+
+			case <-interceptor.ShutdownChannel():
+				fmt.Println("token service shutting down...")
+				goto shutdown
 			}
 		}
 	}
 
+shutdown:
 	if len(s.records) > 0 {
 		for k, v := range s.records {
 			if err := s.persistToDB(k, v); err != nil {
@@ -144,4 +170,7 @@ func (s *TokenService) processERC20Tokens() {
 			delete(s.records, k)
 		}
 	}
+
+	s.database.Close()
+	fmt.Println("token service finished")
 }
--- utils/fileWriter.go
+++ utils/fileWriter.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"math/big"
 	"os"
 )
@@ -20,5 +21,26 @@ func GetCurrentHeight() (uint64, error) {
 		return 0, err
 	}
 	v := big.NewInt(0).SetBytes(data)
+	return v.Uint64(), nil
+}
+
+const tokenProgressFile = "tokenProgress.txt"
+
+func WriteTokenCurrentHeight(height uint64) error {
+	str := big.NewInt(0).SetUint64(height).String()
+	return os.WriteFile(tokenProgressFile, []byte(str), 0666)
+}
+
+func GetTokenCurrentHeight() (uint64, error) {
+	data, err := os.ReadFile(tokenProgressFile)
+	if err != nil {
+		return 0, err
+	}
+
+	v, ok := big.NewInt(0).SetString(string(data), 10)
+	if !ok {
+		return 0, errors.New("get token current height err")
+	}
+
 	return v.Uint64(), nil
 }
